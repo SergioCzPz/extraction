baacd958c806be1ae0347273f964f3857984817e
Iiluminacion GLM <ventasiluminacionglm@gmail.com>
2025-12-18 14:58:54 -0600

    feat: ceil price

--- models/excel_product.py
+++ models/excel_product.py
@@ -1,3 +1,5 @@
+import math
+
 class ExcelProduct:
     def __init__(self, sku: str, title: str, price: float, type: str):
         self.sku = sku
@@ -8,7 +10,8 @@ class ExcelProduct:
     def total_price(self, price: float) -> float:
         first_price = price + (price * 0.01)
         final_price = first_price + (first_price * 0.16)
-        return final_price
+        
+        return float(math.ceil(final_price))
 
     def __repr__(self):
         return f"ExcelProduct(sku='{self.sku}', title={self.title}, type='{self.type}', price={self.price})"
